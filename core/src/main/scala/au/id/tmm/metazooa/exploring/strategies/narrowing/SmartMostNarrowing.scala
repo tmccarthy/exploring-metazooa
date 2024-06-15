2209f3b7f13e1130433e9d65648959757d5f83c0
Timothy McCarthy <ebh042@gmail.com>
2024-06-15 21:09:44 +1000

    Make SmartMostNarrowing construction weaker

--- core/src/main/scala/au/id/tmm/metazooa/exploring/strategies/narrowing/SmartMostNarrowing.scala
+++ core/src/main/scala/au/id/tmm/metazooa/exploring/strategies/narrowing/SmartMostNarrowing.scala
@@ -2,13 +2,13 @@ package au.id.tmm.metazooa.exploring.strategies.narrowing
 
 import au.id.tmm.metazooa.exploring.game.{Move, State}
 import au.id.tmm.metazooa.exploring.strategies.Strategy
-import cats.Monad
+import cats.Applicative
 import cats.syntax.functor.*
 
-class SmartMostNarrowing[F[_] : Monad] private (
+class SmartMostNarrowing[F[_] : Applicative] private (
   narrowingApproach: NarrowingApproach[?],
 ) extends Strategy[F] {
-  override def proposeMove(state: State.VisibleToPlayer): F[Move] = Monad[F].pure {
+  override def proposeMove(state: State.VisibleToPlayer): F[Move] = Applicative[F].pure {
     val (bestGuess, _) = GuessScoring.bestGuess(
       narrowingApproach,
       state,
@@ -21,11 +21,11 @@ class SmartMostNarrowing[F[_] : Monad] private (
 
 object SmartMostNarrowing {
 
-  def apply[F[_] : Monad]: F[SmartMostNarrowing[F]] = apply(NarrowingApproach.MeanLeastRemaining)
+  def apply[F[_] : Applicative]: F[SmartMostNarrowing[F]] = apply(NarrowingApproach.MeanLeastRemaining)
 
-  def apply[F[_] : Monad](narrowingApproach: NarrowingApproach[?]): F[SmartMostNarrowing[F]] =
+  def apply[F[_] : Applicative](narrowingApproach: NarrowingApproach[?]): F[SmartMostNarrowing[F]] =
     for {
-      _ <- Monad[F].pure(())
+      _ <- Applicative[F].pure(())
     } yield new SmartMostNarrowing[F](narrowingApproach)
 
 }
